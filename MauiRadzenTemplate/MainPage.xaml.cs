b427817df4b8e96718f79eb0092701d50b7b1c19
Humberto Molina Lopez <humberto_molinalopez@hotmail.com>
2024-04-09 10:57:18 -0600

    Agregar archivos de proyecto.

---
+++ MauiRadzenTemplate/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRadzenTemplate
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
